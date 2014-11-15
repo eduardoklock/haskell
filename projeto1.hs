b875e560ad64bb31dc655a816f6f54527fb4c619
Eduardo <eduardo.klock@gmail.com>
2014-11-15 14:37:41 -0200

    sometimes i cant understand why

--- projeto1.hs
+++ projeto1.hs
@@ -1,56 +1,43 @@
-module Formas ( Forma (Esfera, Cilindo, Cone, Tronco, Oblato, Prolato),
-                raio, raioBase, raioSeccao, altura, semiEixoMaior, semiEixoMenor) where
+module Formas (Forma (Esfera, Cilindro, Cone, TroncoCone, Oblato, Prolato),
+Raio, RaioBase, RaioSeccao, Altura ,SemiEixoMaior, SemiEixoMenor, area, volume)
+where
 
-    data Forma = 
-        Esfera raio |
-        Cilindro raio altura |
-        Cone raio altura |
-        Tronco raioBase raioSeccao altura |
-        Oblato semiEixoMaior semiEixoMenor |
-        Prolato semiEixoMaior semiEixoMenor
+data Forma = Esfera Raio
+	     | Cilindro Raio Altura
+	     | Cone Raio Altura
+   	     | TroncoCone RaioBase RaioSeccao Altura
+	     | Oblato SemiEixoMaior SemiEixoMenor
+	     | Prolato SemiEixoMaior SemiEixoMenor
+	deriving Show
 
-        deriving Show
-            type raio = Float 
-            type altura = Float
-            type raioBase = Float
-            type raioSeccao = Float
-            type semiEixoMaior = Float
-            type semiEixoMenor = Float
-    
--- formulas area
-area :: Forma -> Float
-area (Esfera raio) = (4*pi)*(raio^2)
-area (Cilindro raio altura) = areaLateralCilindro raio altura + 2*pi*(raio^2)
-area (Cone raio altura) = (pi*raio)*(sqrt(raio^2 + altura^2) + raio)
-area (Tronco raioBase raioSeccao altura) = pi(raioBase^2 + raioSeccao^2) + areaLateralTronco raioBase raioSeccao altura
--- oblato
--- prolato
+type Raio = Float
+type RaioBase = Float
+type RaioSeccao = Float
+type Altura = Float
+type SemiEixoMaior = Float
+type SemiEixoMenor = Float
 
--- formulas volume
+
+--Definindo area lateral para tornar as formulas mais legiveis
+areaLat :: Forma -> Float
+areaLat (Cilindro r h) = 2*pi*r*h
+areaLat (Cone r h) = pi*r *sqrt(r^2+h^2)
+areaLat (TroncoCone r1 r2 h) = pi*(r1+r2)*sqrt(h^2*(r1-r2)^2)
+
+--Definindo as formulas de area
+area :: Forma -> Float 
+area (Esfera r) = 4*pi*r^2
+area (Cilindro r areaLat) = areaLat+2*pi*r^2
+area (Cone r h) = pi*r*sqrt(r^2+h^2)+r
+area (TroncoCone r1 r2 h) = pi*r1^2+pi*r2^2+areaLat (TroncoCone r1 r2 h)
+area (Oblato s1 s2) = 2*pi*s1^2+s2^2/(sqrt(s1^2+s2^2)/s1)+log(1+(sqrt(s1^2+s2^2)/s1)/1-(sqrt(s1^2+s2^2)/s1))
+area (Prolato s1 s2) = 2*pi*s2^2+2*pi*(s1*s2/(sqrt(s1^2+s2^2)/s1))*asin(sqrt(s1^2+s2^2)/s1)   
+
+--Definindo as formulas de volume
 volume :: Forma -> Float
-volume (Esfera raio) = (4*pi*(raio^3))/3
-volume (Cilindro raio altura)  = pi*(raio^2)*altura
-volume (Cone raio altura) = (1*pi*(raio^2)*altura)/3
-volume (Tronco raioBase raioSeccao altura) = pi*altura*(raioBase^2 + raioSeccao^2 + raioBase*raioSeccao)
--- oblato
--- proplato
-
--- formulas area lateral
-
--- formulas cilindro
-
-areaLateralCilindro raio altura = 2*pi*raio*altura
-
--- formula cone
-
-areaLateralCone raio altura = (pi*raio)*(sqrt (raio^2 + altura^2))
-
--- formula tronco
-
-areaLateralTronco raioBase raioSeccao altura = pi*(raioBase + raioSeccao)*(sqrt(altura^2 + raioBase^2 + raioSeccao^2))
-
---formula esferoide oblato
- 
-excentricidade semiEixoMaior semiEixoMenor = (sqrt(semiEixoMaior^2 + semiEixoMenor^2))/semiEixoMaior
-
-
+volume (Esfera r) = 4/3*pi*(r^3)
+volume (Cilindro r h) = pi*(r^2)*h
+volume (Cone r h) = 1/3*pi*(r^2)*h
+volume (TroncoCone r1 r2 h)  = 1/3*pi*h*((r1^2)+(r2^2)+r1*r2)
+volume (Oblato r1 r2) = 4/3*pi*(r1^2)*r2
+volume (Prolato r1 r2) = 4/3*pi*r1*r2^2
