8c8ab514d08848f37a4a2c69e4bb666a403d6d27
Eduardo Klock <eduardo.klock@gmail.com>
2014-11-14 22:37:48 -0200

    something good

--- projeto1.hs
+++ projeto1.hs
@@ -1,35 +1,56 @@
--- formulas circulo
+module Formas ( Forma (Esfera, Cilindo, Cone, Tronco, Oblato, Prolato),
+                raio, raioBase, raioSeccao, altura, semiEixoMaior, semiEixoMenor) where
 
-areaCirculo raio = (4*pi)*(raio^2)
+    data Forma = 
+        Esfera raio |
+        Cilindro raio altura |
+        Cone raio altura |
+        Tronco raioBase raioSeccao altura |
+        Oblato semiEixoMaior semiEixoMenor |
+        Prolato semiEixoMaior semiEixoMenor
 
-volumeCirculo raio = (4*pi*(raio^3))/3
+        deriving Show
+            type raio = Float 
+            type altura = Float
+            type raioBase = Float
+            type raioSeccao = Float
+            type semiEixoMaior = Float
+            type semiEixoMenor = Float
+    
+-- formulas area
+area :: Forma -> Float
+area (Esfera raio) = (4*pi)*(raio^2)
+area (Cilindro raio altura) = areaLateralCilindro raio altura + 2*pi*(raio^2)
+area (Cone raio altura) = (pi*raio)*(sqrt(raio^2 + altura^2) + raio)
+area (Tronco raioBase raioSeccao altura) = pi(raioBase^2 + raioSeccao^2) + areaLateralTronco raioBase raioSeccao altura
+-- oblato
+-- prolato
+
+-- formulas volume
+volume :: Forma -> Float
+volume (Esfera raio) = (4*pi*(raio^3))/3
+volume (Cilindro raio altura)  = pi*(raio^2)*altura
+volume (Cone raio altura) = (1*pi*(raio^2)*altura)/3
+volume (Tronco raioBase raioSeccao altura) = pi*altura*(raioBase^2 + raioSeccao^2 + raioBase*raioSeccao)
+-- oblato
+-- proplato
+
+-- formulas area lateral
 
 -- formulas cilindro
 
 areaLateralCilindro raio altura = 2*pi*raio*altura
 
-areaTotalCilindro raio altura = areaLateralCilindro raio altura + 2*pi*(raio^2)
-
-volumeCilindro raio altura  = pi*(raio^2)*altura
-
 -- formula cone
 
 areaLateralCone raio altura = (pi*raio)*(sqrt (raio^2 + altura^2))
-
-areaTotalCone raio altura = (pi*raio)*(sqrt(raio^2 + altura^2) + raio)
-
-volumeCone raio altura = (1*pi*(raio^2)*altura)/3
 
 -- formula tronco
 
 areaLateralTronco raioBase raioSeccao altura = pi*(raioBase + raioSeccao)*(sqrt(altura^2 + raioBase^2 + raioSeccao^2))
 
-areaTotalTronco raioBase raioSeccao altura = pi(raioBase^2 + raioSeccao^2) + areaLateralTronco raioBase raioSeccao altura
-
-volumeTronco raioBase raioSeccao altura = pi*altura*(raioBase^2 + raioSeccao^2 + raioBase*raioSeccao)
-
 --formula esferoide oblato
  
-excentricidade semiEixoMaior semiEixoMeno = (sqrt(semiEixoMaior^2 + semiEixoMenor^2))/semiEixoMaior
+excentricidade semiEixoMaior semiEixoMenor = (sqrt(semiEixoMaior^2 + semiEixoMenor^2))/semiEixoMaior
 
 
